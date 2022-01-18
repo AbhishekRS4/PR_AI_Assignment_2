6cc42801345d54e0ccd19bf1cee30c125e24b615
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2022-01-18 12:48:50 +0100

    updated code for image classification experiments

--- src/image_classification_experiments.py
+++ src/image_classification_experiments.py
@@ -3,11 +3,101 @@ import numpy as np
 import pandas as pd
 
 from learning_utils import get_standard_scaler, get_tfidf_transformer
-from learning_utils import get_learning_pipeline, compute_classification_metrics_test_data
 from learning_utils import get_knn_classifier, get_grid_search_classifier, get_adaboost_classifier
+from learning_utils import get_learning_pipeline, compute_classification_metrics_test_data, print_classification_metrics
 
-def do_cv_image_classification(dir_bovw_data="bovw_features_data/", which_classifier="knn", end_num_visual_words=None):
-    start_num_visual_words = 50
+def test_knn_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="image_classification_gs_cv_knn.csv"):
+    file_csv_gs_cv = os.path.join(dir_bovw_data, file_csv_gs_cv)
+    df_gs_cv = pd.read_csv(df_gs_cv)
+
+    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
+    test_y = np.load(os.path.join(dir_bovw_data, "test_labels.npy"))
+
+    list_train_acc = []
+    list_test_acc = []
+    list_f1 = []
+
+    for idx_clf in range(df_gs_cv.shape(0)):
+        num_visual_words = df_gs_cv.num_visual_words[idx_clf]
+        preprocess = df_gs_cv.preprocess[idx_clf]
+        metric = df_gs_cv.preprocess[idx_clf]
+        n_neighbors = df_gs_cv.n_neighbors[idx_clf]
+        weights = df_gs_cv.n_neighbors[weights]
+
+        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
+        test_x = np.load(os.path.join(dir_bovw_data, f"test_kmeans_{num_visual_words}.npy"))
+
+        if preprocess == "std_scaler":
+            preprocessor = get_standard_scaler()
+        elif preprocess == "tf_idf":
+            preprocessor = get_tfidf_transformer()
+        else:
+            print(f"wrong option for preprocess: {preprocess} found, so exiting....")
+            return
+
+        classifier = get_knn_classifier(n_neighbors=n_neighbors, metric=metric, weights=weights)
+        pipeline = [("std_scaler", preprocessor), ("knn", classifier)]
+        learning_pipeline = get_learning_pipeline(pipeline)
+        print("learning pipeline")
+        print(learning_pipeline)
+
+        learning_pipeline.fit(train_x, train_y)
+        train_acc = learning_pipeline.score(train_x, train_y)
+        test_pred = learning_pipeline.predict(test_x)
+        test_pred_prob = learning_pipeline.predict_proba(test_x)
+        test_acc, test_f1, test_cm, test_auc = compute_classification_metrics_test_data(test_y, test_pred, test_pred_prob)
+        print(f"train acc: {train_acc:.4f}")
+        print_classification_metrics(test_acc, test_f1, test_cm)
+    return
+
+def test_adaboost_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="image_classification_gs_cv_knn.csv"):
+    file_csv_gs_cv = os.path.join(dir_bovw_data, file_csv_gs_cv)
+    df_gs_cv = pd.read_csv(df_gs_cv)
+
+    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
+    test_y = np.load(os.path.join(dir_bovw_data, "test_labels.npy"))
+
+    list_train_acc = []
+    list_test_acc = []
+    list_f1 = []
+
+    for idx_clf in range(df_gs_cv.shape(0)):
+        num_visual_words = df_gs_cv.num_visual_words[idx_clf]
+        preprocess = df_gs_cv.preprocess[idx_clf]
+        metric = df_gs_cv.preprocess[idx_clf]
+        n_neighbors = df_gs_cv.n_neighbors[idx_clf]
+        weights = df_gs_cv.n_neighbors[weights]
+
+        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
+        test_x = np.load(os.path.join(dir_bovw_data, f"test_kmeans_{num_visual_words}.npy"))
+
+        if preprocess == "std_scaler":
+            preprocessor = get_standard_scaler()
+        elif preprocess == "tf_idf":
+            preprocessor = get_tfidf_transformer()
+        else:
+            print(f"wrong option for preprocess: {preprocess} found, so exiting....")
+            return
+
+        classifier = get_knn_classifier(n_neighbors=n_neighbors, metric=metric, weights=weights)
+        pipeline = [("std_scaler", preprocessor), ("knn", classifier)]
+        learning_pipeline = get_learning_pipeline(pipeline)
+        print("learning pipeline")
+        print(learning_pipeline)
+
+        learning_pipeline.fit(train_x, train_y)
+        train_acc = learning_pipeline.score(train_x, train_y)
+        test_pred = learning_pipeline.predict(test_x)
+        test_pred_prob = learning_pipeline.predict_proba(test_x)
+        test_acc, test_f1, test_cm, test_auc = compute_classification_metrics_test_data(test_y, test_pred, test_pred_prob)
+        print(f"train acc: {train_acc:.4f}")
+        print_classification_metrics(test_acc, test_f1, test_cm)
+    return
+
+
+def do_cv_image_classification(dir_bovw_data="bovw_features_data/", which_classifier="knn", start_num_visual_words=None, end_num_visual_words=None):
+    if start_num_visual_words is None:
+        start_num_visual_words = 50
     if end_num_visual_words is None:
         end_num_visual_words = 205
 
@@ -23,10 +113,10 @@ def do_cv_image_classification(dir_bovw_data="bovw_features_data/", which_classi
         param_grid = {
             "base_estimator__criterion" : ["gini", "entropy"],
             "base_estimator__splitter" :   ["best", "random"],
-            "base_estimator__max_depth" : np.arange(4, 13),
+            "base_estimator__max_depth" : np.arange(5, 11),
             "base_estimator__min_samples_split" : np.arange(2, 5),
             "n_estimators" : np.arange(10, 160, 10),
-            "learning_rate" : [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1],
+            "learning_rate" : [0.01, 0.1, 1],
         }
     else:
         print(f"wrong option : {which_classifier}")
@@ -47,8 +137,8 @@ def do_cv_image_classification(dir_bovw_data="bovw_features_data/", which_classi
             classifier = get_adaboost_classifier()
 
         list_pipelines = [
-            [("scaler", get_standard_scaler()), ("grid_search", get_grid_search_classifier(classifier, param_grid))],
-            [("tfidf_transformer", get_tfidf_transformer()), ("grid_search", get_grid_search_classifier(classifier, param_grid))],
+            [("std_scaler", get_standard_scaler()), ("grid_search", get_grid_search_classifier(classifier, param_grid))],
+            [("tf_idf", get_tfidf_transformer()), ("grid_search", get_grid_search_classifier(classifier, param_grid))],
         ]
 
         for pipeline_idx in range(len(list_pipelines)):
