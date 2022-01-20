6da0345cda1e95a240e5da2ef4e558ef6f7b0cda
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2022-01-20 13:08:09 +0100

    updated script used for cross validation and test experiments with sift + bovw features

--- src/image_classification_sift_bovw_experiments.py
+++ src/image_classification_sift_bovw_experiments.py
@@ -9,17 +9,17 @@ from learning_utils import get_standard_scaler, get_tfidf_transformer, get_accur
 from learning_utils import get_learning_pipeline, compute_classification_metrics_test_data, print_classification_metrics
 from learning_utils import get_knn_classifier, get_adaboost_classifier, get_gaussian_nb_classifier, get_grid_search_classifier
 
-def test_knn_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="image_classification_gs_cv_knn.csv"):
-    file_csv_gs_cv = os.path.join(dir_bovw_data, file_csv_gs_cv)
+def test_knn_classifier_sift(dir_sift_data="sift_features/", file_csv_gs_cv="image_classification_gs_cv_knn.csv"):
+    file_csv_gs_cv = os.path.join(dir_sift_data, file_csv_gs_cv)
     df_gs_cv = pd.read_csv(file_csv_gs_cv)
 
-    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
-    test_y = np.load(os.path.join(dir_bovw_data, "test_labels.npy"))
+    train_y = np.load(os.path.join(dir_sift_data, "train_labels.npy"))
+    test_y = np.load(os.path.join(dir_sift_data, "test_labels.npy"))
 
-    file_log_results=f"image_classification_test_knn.csv"
-    test_col_names = ["clustering_algo", "num_visual_words", "preprocess", "classifier", "n_neighbors", "metric", "weights", "test_acc", "test_f1", "test_roc_auc"]
+    file_log_results = "image_classification_test_knn.csv"
+    test_col_names = ["features", "num_visual_words", "preprocess", "classifier", "n_neighbors", "metric", "weights", "test_acc", "test_f1", "test_roc_auc"]
     test_results_rows = []
-    csv_writer = CSVWriter(os.path.join(dir_bovw_data, file_log_results), test_col_names)
+    csv_writer = CSVWriter(os.path.join(dir_sift_data, file_log_results), test_col_names)
 
     for idx_clf in range(len(df_gs_cv)):
         num_visual_words = df_gs_cv.num_visual_words[idx_clf]
@@ -28,8 +28,8 @@ def test_knn_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="ima
         n_neighbors = df_gs_cv.n_neighbors[idx_clf]
         weights = df_gs_cv.weights[idx_clf]
 
-        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
-        test_x = np.load(os.path.join(dir_bovw_data, f"test_kmeans_{num_visual_words}.npy"))
+        train_x = np.load(os.path.join(dir_sift_data, f"train_sift_{num_visual_words}.npy"))
+        test_x = np.load(os.path.join(dir_sift_data, f"test_sift_{num_visual_words}.npy"))
 
         if preprocess == "std_scaler":
             preprocessor = get_standard_scaler()
@@ -40,7 +40,7 @@ def test_knn_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="ima
             return
 
         classifier = get_knn_classifier(n_neighbors=n_neighbors, metric=metric, weights=weights)
-        pipeline = [("std_scaler", preprocessor), ("knn", classifier)]
+        pipeline = [(preprocess, preprocessor), ("knn", classifier)]
         learning_pipeline = get_learning_pipeline(pipeline)
         print(f"num visual words : {num_visual_words}")
         print(f"learning pipeline : {pipeline}")
@@ -50,30 +50,39 @@ def test_knn_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="ima
         test_pred_prob = learning_pipeline.predict_proba(test_x)
         test_acc, test_f1, test_roc_auc, test_cm = compute_classification_metrics_test_data(test_y, test_pred, test_pred_prob)
         print_classification_metrics(test_acc, test_f1, test_roc_auc, test_cm)
-        row_ = ["kmeans", num_visual_words, preprocess, "knn", n_neighbors, metric, weights, round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)]
+        row_ = ["sift", num_visual_words, preprocess, "knn", n_neighbors, metric, weights, round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)]
         csv_writer.write_row(row_)
     csv_writer.close()
     return
 
-def test_adaboost_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv="image_classification_gs_cv_adaboost.csv"):
-    file_csv_gs_cv = os.path.join(dir_bovw_data, file_csv_gs_cv)
-    df_gs_cv = pd.read_csv(df_gs_cv)
+def test_adaboost_classifier_sift(dir_sift_data="sift_features/", file_csv_gs_cv="image_classification_gs_cv_adaboost.csv"):
+    file_csv_gs_cv = os.path.join(dir_sift_data, file_csv_gs_cv)
+    df_gs_cv = pd.read_csv(file_csv_gs_cv)
 
-    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
-    test_y = np.load(os.path.join(dir_bovw_data, "test_labels.npy"))
+    train_y = np.load(os.path.join(dir_sift_data, "train_labels.npy"))
+    test_y = np.load(os.path.join(dir_sift_data, "test_labels.npy"))
 
-    file_log_results=f"image_classification_test_adaboost.csv"
-    test_col_names = ["clustering_algo", "num_visual_words", "preprocess", "classifier", "", "", "", "", "", "", "test_acc", "test_f1", "test_roc_auc"]
+    file_log_results = "image_classification_test_adaboost.csv"
+    test_col_names = [
+        "features", "num_visual_words", "preprocess", "classifier",
+        "base_estimator__criterion", "base_estimator__max_depth", "base_estimator__min_samples_split", "base_estimator__splitter",
+        "learning_rate", "n_estimators", "test_acc", "test_f1", "test_roc_auc"
+    ]
     test_results_rows = []
-    csv_writer = CSVWriter(os.path.join(dir_bovw_data, file_log_results), cv_col_names)
+    csv_writer = CSVWriter(os.path.join(dir_sift_data, file_log_results), test_col_names)
 
     for idx_clf in range(len(df_gs_cv)):
         num_visual_words = df_gs_cv.num_visual_words[idx_clf]
         preprocess = df_gs_cv.preprocess[idx_clf]
+        criterion = df_gs_cv.base_estimator__criterion[idx_clf]
+        max_depth = df_gs_cv.base_estimator__max_depth[idx_clf]
+        min_samples_split = df_gs_cv.base_estimator__min_samples_split[idx_clf]
+        splitter = df_gs_cv.base_estimator__splitter[idx_clf]
+        learning_rate = df_gs_cv.learning_rate[idx_clf]
+        n_estimators = df_gs_cv.n_estimators[idx_clf]
 
-
-        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
-        test_x = np.load(os.path.join(dir_bovw_data, f"test_kmeans_{num_visual_words}.npy"))
+        train_x = np.load(os.path.join(dir_sift_data, f"train_sift_{num_visual_words}.npy"))
+        test_x = np.load(os.path.join(dir_sift_data, f"test_sift_{num_visual_words}.npy"))
 
         if preprocess == "std_scaler":
             preprocessor = get_standard_scaler()
@@ -83,8 +92,11 @@ def test_adaboost_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv
             print(f"wrong option for preprocess: {preprocess} found, so exiting....")
             return
 
-        classifier = get_adaboost_classifier()
-        pipeline = [("std_scaler", preprocessor), ("adaboost", classifier)]
+        classifier = get_adaboost_classifier(
+            n_estimators=n_estimators, learning_rate=learning_rate, criterion=criterion,
+            max_depth=max_depth, min_samples_split=min_samples_split, splitter=splitter
+        )
+        pipeline = [(preprocess, preprocessor), ("adaboost", classifier)]
         learning_pipeline = get_learning_pipeline(pipeline)
         print("learning pipeline")
         print(learning_pipeline)
@@ -94,18 +106,21 @@ def test_adaboost_classifier(dir_bovw_data="bovw_features_data/", file_csv_gs_cv
         test_pred_prob = learning_pipeline.predict_proba(test_x)
         test_acc, test_f1, test_roc_auc, test_cm = compute_classification_metrics_test_data(test_y, test_pred, test_pred_prob)
         print_classification_metrics(test_acc, test_f1, test_roc_auc, test_cm)
-        row_ = ["kmeans", num_visual_words, preprocess, "adaboost", n_neighbors, metric, weights, round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)]
+        row_ = [
+            "sift", num_visual_words, preprocess, "adaboost", criterion, max_depth, min_samples_split, splitter,
+            learning_rate, n_estimators, round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)
+        ]
         csv_writer.write_row(row_)
     csv_writer.close()
     return
 
-def do_gs_cv_image_classification(dir_bovw_data="bovw_features_data/", which_classifier="knn", start_num_visual_words=None, end_num_visual_words=None):
+def do_gs_cv_image_classification_sift(dir_sift_data="sift_features/", which_classifier="knn", start_num_visual_words=None, end_num_visual_words=None):
     if start_num_visual_words is None:
         start_num_visual_words = 5
     if end_num_visual_words is None:
         end_num_visual_words = 205
 
-    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
+    train_y = np.load(os.path.join(dir_sift_data, "train_labels.npy"))
 
     if which_classifier == "knn":
         param_grid = {
@@ -129,12 +144,12 @@ def do_gs_cv_image_classification(dir_bovw_data="bovw_features_data/", which_cla
     list_all_params = sorted(list(param_grid.keys()))
 
     file_log_results=f"image_classification_gs_cv_{which_classifier}.csv"
-    cv_col_names = ["clustering_algo", "num_visual_words", "preprocess", "classifier"] + list_all_params + ["best_cv_score"]
+    cv_col_names = ["features", "num_visual_words", "preprocess", "classifier"] + list_all_params + ["best_cv_score"]
     cv_results_rows = []
-    csv_writer = CSVWriter(os.path.join(dir_bovw_data, file_log_results), cv_col_names)
+    csv_writer = CSVWriter(os.path.join(dir_sift_data, file_log_results), cv_col_names)
 
     for num_visual_words in range(start_num_visual_words, end_num_visual_words, 5):
-        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
+        train_x = np.load(os.path.join(dir_sift_data, f"train_sift_{num_visual_words}.npy"))
 
         if which_classifier == "knn":
             classifier = get_knn_classifier()
@@ -164,59 +179,59 @@ def do_gs_cv_image_classification(dir_bovw_data="bovw_features_data/", which_cla
             for param_key in list_all_params:
                 param_values.append(learning_pipeline["grid_search"].best_params_[param_key])
 
-            row_ = ["kmeans", num_visual_words, preprocess_method, which_classifier] \
+            row_ = ["sift", num_visual_words, preprocess_method, which_classifier] \
                 + param_values \
                 + [round(learning_pipeline["grid_search"].best_score_, 4)]
             csv_writer.write_row(row_)
     csv_writer.close()
     return
 
-def do_cv_nb_image_classification(dir_bovw_data, start_num_visual_words=None, end_num_visual_words=None):
+def do_cv_nb_image_classification_sift(dir_sift_data, start_num_visual_words=None, end_num_visual_words=None):
     if start_num_visual_words is None:
         start_num_visual_words = 5
     if end_num_visual_words is None:
         end_num_visual_words = 205
 
     scoring_fn = get_accuracy_scoring_fn()
-    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
+    train_y = np.load(os.path.join(dir_sift_data, "train_labels.npy"))
 
     file_log_results = "image_classification_cv_gaussian_nb.csv"
-    cv_col_names = ["clustering_algo", "num_visual_words", "preprocess", "classifier", "best_cv_score"]
+    cv_col_names = ["features", "num_visual_words", "preprocess", "classifier", "best_cv_score"]
     cv_results_rows = []
-    csv_writer = CSVWriter(os.path.join(dir_bovw_data, file_log_results), cv_col_names)
+    csv_writer = CSVWriter(os.path.join(dir_sift_data, file_log_results), cv_col_names)
 
     for num_visual_words in range(start_num_visual_words, end_num_visual_words, 5):
-        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
+        train_x = np.load(os.path.join(dir_sift_data, f"train_sift_{num_visual_words}.npy"))
         preprocess_method = "std_scaler"
         pipeline = [(preprocess_method, get_standard_scaler()), ("gaussian_nb", get_gaussian_nb_classifier())]
         learning_pipeline = get_learning_pipeline(pipeline)
         clf_cv = cross_validate(learning_pipeline, train_x, train_y, cv=5, scoring=scoring_fn)
 
-        row_ = ["kmeans", num_visual_words, preprocess_method, "gaussian_nb"] \
+        row_ = ["sift", num_visual_words, preprocess_method, "gaussian_nb"] \
             + [round(np.mean(clf_cv["test_score"]), 4)]
         csv_writer.write_row(row_)
     csv_writer.close()
     return
 
-def test_nb_image_classifier(dir_bovw_data, start_num_visual_words=None, end_num_visual_words=None):
+def test_nb_image_classifier_sift(dir_sift_data, start_num_visual_words=None, end_num_visual_words=None):
     if start_num_visual_words is None:
         start_num_visual_words = 5
     if end_num_visual_words is None:
         end_num_visual_words = 205
 
     scoring_fn = get_accuracy_scoring_fn()
-    train_y = np.load(os.path.join(dir_bovw_data, "train_labels.npy"))
-    test_y = np.load(os.path.join(dir_bovw_data, "test_labels.npy"))
+    train_y = np.load(os.path.join(dir_sift_data, "train_labels.npy"))
+    test_y = np.load(os.path.join(dir_sift_data, "test_labels.npy"))
 
     file_log_results = "image_classification_test_gaussian_nb.csv"
-    test_col_names = ["clustering_algo", "num_visual_words", "preprocess", "classifier", "test_acc", "test_f1", "test_roc_auc"]
+    test_col_names = ["features", "num_visual_words", "preprocess", "classifier", "test_acc", "test_f1", "test_roc_auc"]
     test_results_rows = []
-    csv_writer = CSVWriter(os.path.join(dir_bovw_data, file_log_results), test_col_names)
+    csv_writer = CSVWriter(os.path.join(dir_sift_data, file_log_results), test_col_names)
 
     for num_visual_words in range(start_num_visual_words, end_num_visual_words, 5):
         print(f"num visual words : {num_visual_words}")
-        train_x = np.load(os.path.join(dir_bovw_data, f"train_kmeans_{num_visual_words}.npy"))
-        test_x = np.load(os.path.join(dir_bovw_data, f"test_kmeans_{num_visual_words}.npy"))
+        train_x = np.load(os.path.join(dir_sift_data, f"train_sift_{num_visual_words}.npy"))
+        test_x = np.load(os.path.join(dir_sift_data, f"test_sift_{num_visual_words}.npy"))
 
         preprocess_method = "std_scaler"
         pipeline = [(preprocess_method, get_standard_scaler()), ("gaussian_nb", get_gaussian_nb_classifier())]
@@ -226,7 +241,7 @@ def test_nb_image_classifier(dir_bovw_data, start_num_visual_words=None, end_num
         test_pred_prob = learning_pipeline.predict_proba(test_x)
         test_acc, test_f1, test_roc_auc, test_cm = compute_classification_metrics_test_data(test_y, test_pred, test_pred_prob)
 
-        row_ = ["kmeans", num_visual_words, preprocess_method, "gaussian_nb", round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)]
+        row_ = ["sift", num_visual_words, preprocess_method, "gaussian_nb", round(test_acc, 4), round(test_f1, 4), round(test_roc_auc, 4)]
         print_classification_metrics(test_acc, test_f1, test_roc_auc, test_cm)
         csv_writer.write_row(row_)
     csv_writer.close()
