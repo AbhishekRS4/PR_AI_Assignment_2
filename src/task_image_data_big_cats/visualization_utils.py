a4aea13c8014fefb71132ef203c2a1d61b4f1e96
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2022-01-25 23:36:19 +0100

    added plot marker and grid in the plots

--- src/task_image_data_big_cats/visualization_utils.py
+++ src/task_image_data_big_cats/visualization_utils.py
@@ -15,11 +15,12 @@ def plot_compare_scores(arr_num_visual_words, arr_scores_1, arr_scores_2, str_ti
     str_y_label = "Accuracy Score"
 
     fig = plt.figure(figsize=(12, 9))
-    plt.plot(arr_num_visual_words, arr_scores_1, label=str_label_plot_1)
-    plt.plot(arr_num_visual_words, arr_scores_2, label=str_label_plot_2)
+    plt.plot(arr_num_visual_words, arr_scores_1, "o--", label=str_label_plot_1)
+    plt.plot(arr_num_visual_words, arr_scores_2, "o--", label=str_label_plot_2)
     plt.title(str_title)
     plt.xlabel(str_x_label)
     plt.ylabel(str_y_label)
+    plt.grid()
     plt.legend()
     fig.show()
     return
